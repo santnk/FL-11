ca4aff6e7de3ef253965e5118d85951660b0adcf
Serhii Antonenko <santonenko87@gmail.com>
2019-07-09 23:00:36 +0300

    final version

--- FL11_HW7/homework/src/js/task2.js
+++ FL11_HW7/homework/src/js/task2.js
@@ -15,36 +15,35 @@ let numberGuesses = 0;
 let attempt = 3;
 
 wannaPlay = confirm('Do you want to play a game?');
-  if (!confirm ) {
-    alert('You did not become a billionaire, but can.');
-  } else {
-    randomNumber = Math.floor(Math.random() * rangeUpTo + 1);
-    for (let i = 0; i < attempt; i++) {
-      numberGuesses = parseInt(prompt('Choose a number from 0 to ' +
-          +rangeUpTo + '\n Number of attempts that you have: ' + counter + '\n Total prize: ' + totalPrize +
-          '\n Possible prize on current attempt: ' + prize, ''));
-      counter -= 1;
-    if (numberGuesses === randomNumber) {
-        totalPrize += prize;
-        alert('You have won ' + prize + ' $');
-        break;
-    } else {
-        prize = prize - prizeDecrise;
-    }
-
+if (!wannaPlay ) {
+  alert('You did not become a billionaire, but can.');
+} else {
+  randomNumber = Math.floor(Math.random() * rangeUpTo + 1);
+  for (let i = 0; i < attempt; i++) {
+    numberGuesses = parseInt(prompt('Choose a number from 0 to ' + rangeUpTo + '\n Number of attempts that you have: ' + counter + '\n Total prize: ' + totalPrize +
+        '\n Possible prize on current attempt: ' + prize, ''));
+    counter -= 1;
     if (counter === 0) {
       alert('You lose. Thank you for your participation.');
       break;
-  } else {
-        playAgain = confirm('Congratulation, you won! Your prize is:' + totalPrize +
-          ' $. Do you want to continue?');
-        if (playAgain === false) {
-          alert('Thank you for your participation. Your prize is: ' + totalPrize + ' $');
-          break;
+    }
+    if (numberGuesses === randomNumber) {
+      totalPrize += prize;
+      alert('You have won ' + prize + ' $');
+      playAgain = confirm('Congratulation, you won! Your prize is:' + totalPrize + ' $. Do you want to continue?');
+      if (playAgain === false) {
+        break;
       } else {
         rangeUpTo += rangIncrease;
         prize *= prizeIncrease;
         continue;
+      }
+    } else {
+      prize = prize - prizeDecrise;
+      playAgain = confirm('You were wrong! Your prize is:' + prize + ' $. Do you want to continue?');
+      if (!playAgain) {
+        alert('Thank you for your participation. Your prize is: ' + totalPrize + ' $');
+        break;
       }
     }
   }
